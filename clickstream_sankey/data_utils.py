05adbc9dd7e98d34fe12c97a3eb3758a0ea45741
dannyycwang <55090248+dannyycwang@users.noreply.github.com>
2025-06-06 11:54:19 +0200

    Update data_utils.py

--- clickstream_sankey/data_utils.py
+++ clickstream_sankey/data_utils.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import ast
+
+def safe_eval(val):
+    try:
+        return ast.literal_eval(val)
+    except:
+        return val
+
+def load_and_preprocess(filepath):
+    df = pd.read_csv(filepath)
+    df['truncated_sequence'] = df['truncated_sequence'].apply(safe_eval)
+    return df
 
